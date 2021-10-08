a32e1c506e64bf5924710b5ed432713bfddde261
krajan <krishnaveni07.rajan@gmail.com>
2021-10-08 18:55:26 +0530

    Commit Telephone Class

--- src/AccessMod/TClassM.java
+++ src/AccessMod/TClassM.java
@@ -1,3 +1,9 @@
+/*
+Assignment 4:
+We have to calculate the percentage of marks obtained in three subjects (each out of 100) by student A and in four subjects (each out of 100) by student B. Create an abstract class 'Marks' with an abstract method 'getPercentage'. It is inherited by two other classes 'A' and 'B' each having a method with the same name which returns the percentage of the students. The constructor of student A takes the marks in three subjects as its parameters and 
+the marks in four subjects as its parameters for student B. Create an object for each of the two classes and 
+print the percentage of marks for both the students.
+*/
 package AccessMod;
 
 public class TClassM {
@@ -31,5 +37,13 @@ public class TClassM {
 		System.out.println("Value of private integer variable deptId = " + percentage);
 		System.out.println("Value of default variable limitCap = " + limitCap);
 		System.out.println("Value of protected variable = " + percentage);
+	}
+
+	public int getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
 	}
 }
---
+++ src/Oct3/telephoneClass.java
@@ -0,0 +1,40 @@
+/*Assignment 4:
+Create a class telephone with ( ) , lift ( ) and disconnected ( ) methods as abstract methods 
+create another class smart telephone and demonstrate polymorphism 
+*/
+package Oct3;
+
+abstract class telephone{
+	abstract void ring();
+	abstract void lift();
+	abstract void disconnected();	
+}
+
+ class smartphone extends telephone {
+	@Override
+	public void ring() {
+		System.out.println("Phone rings during incoming call"+ "\n");	
+	}
+
+	@Override
+	public void lift() {
+		System.out.println("Phone lifted by the user as the phone rang"+ "\n");
+	}
+
+	@Override
+	public void disconnected() {
+		System.out.println("Phone call disconnected after user talks"+ "\n");
+	}
+	
+}
+
+public class telephoneClass {
+	
+	public static void main(String[] args) {
+		telephone ph = new smartphone();
+		ph.ring();
+		ph.lift();
+		ph.disconnected();
+		}
+
+}
